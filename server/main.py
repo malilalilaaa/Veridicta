d58a6e1375ea22a0318f2661a10d6c65fbe9af15
malilalilaaa <shafwatmahiya1210@gmail.com>
2025-05-23 01:44:25 +0600

    feat: implement chat endpoint with query handling

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from server.pydantic_models.chat_body import ChatBody
 
 app = FastAPI()
 
 # send quary
 @app.post("/chat")
-def chat_endpoint():
-    pass
+def chat_endpoint(body: ChatBody):
+    print(body.query)
+    return body.query
