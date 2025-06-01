54955c871348b23b71df5383093bd39a3b5f14bc
malilalilaaa <shafwatmahiya1210@gmail.com>
2025-06-01 23:47:51 +0600

    feat: add file uploader widget for attaching evidence in crime reports

---
+++ lib/widgets/crime_report/file_uploader.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+class FileUploaderWidget extends StatefulWidget {
+  const FileUploaderWidget({super.key});
+
+  @override
+  State<FileUploaderWidget> createState() => _FileUploaderWidgetState();
+}
+
+class _FileUploaderWidgetState extends State<FileUploaderWidget> {
+  List<PlatformFile> _files = [];
+
+  Future<void> _pickFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+    );
+
+    if (result != null) {
+      setState(() {
+        _files = result.files;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Attach Evidence (Optional)',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 12),
+        GestureDetector(
+          onTap: _pickFiles,
+          child: Center(
+            child: Text(
+              'Drop files here or click to upload',
+              style: TextStyle(color: Colors.grey[400]),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 12),
+        if (_files.isNotEmpty)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+                _files.map((file) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Text(
+                      file.name,
+                      style: TextStyle(color: Colors.grey[400], fontSize: 13),
+                    ),
+                  );
+                }).toList(),
+          ),
+      ],
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import gal
 import local_auth_darwin
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
